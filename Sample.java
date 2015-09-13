c64fb824ecfbcd0d2abf54877e6e898e128689a4
umar85 <mduf1985@gmail.com>
2015-09-13 21:59:12 +0530

    Pushing Sample java file

---
+++ Sample.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a,b;
+Scanner scan=new Scanner(System.in);
+int c=scan.nextInt();
+System.out.println(c*4);
+	}
+
+}
